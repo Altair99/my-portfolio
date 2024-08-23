fd6b4fac3e9102309d6fa815d0598336618d67f2
LuisVera <luis.v.priego@gmail.com>
2024-08-22 18:11:05 -0600

    update astro and change build directory

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,9 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://altair99.github.io',
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  build: {
+    assets: 'assets'
+  }
+
 });
